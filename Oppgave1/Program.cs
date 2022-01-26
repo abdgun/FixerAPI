d866ae6f7f658e5d49748f45bc29a5e5a8641376
abdgun <abdgun@gmail.com>
2022-01-27 00:20:16 +0100

    Add project files.

---
+++ Oppgave1/Program.cs
@@ -0,0 +1,300 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace FixerAPI
+{
+
+    public class Rates
+    {
+        public double AED { get; set; }
+        public double AFN { get; set; }
+        public double ALL { get; set; }
+        public double AMD { get; set; }
+        public double ANG { get; set; }
+        public double AOA { get; set; }
+        public double ARS { get; set; }
+        public double AUD { get; set; }
+        public double AWG { get; set; }
+        public double AZN { get; set; }
+        public double BAM { get; set; }
+        public double BBD { get; set; }
+        public double BDT { get; set; }
+        public double BGN { get; set; }
+        public double BHD { get; set; }
+        public double BIF { get; set; }
+        public double BMD { get; set; }
+        public double BND { get; set; }
+        public double BOB { get; set; }
+        public double BRL { get; set; }
+        public double BSD { get; set; }
+        public double BTC { get; set; }
+        public double BTN { get; set; }
+        public double BWP { get; set; }
+        public double BYN { get; set; }
+        public double BYR { get; set; }
+        public double BZD { get; set; }
+        public double CAD { get; set; }
+        public double CDF { get; set; }
+        public double CHF { get; set; }
+        public double CLF { get; set; }
+        public double CLP { get; set; }
+        public double CNY { get; set; }
+        public double COP { get; set; }
+        public double CRC { get; set; }
+        public double CUC { get; set; }
+        public double CUP { get; set; }
+        public double CVE { get; set; }
+        public double CZK { get; set; }
+        public double DJF { get; set; }
+        public double DKK { get; set; }
+        public double DOP { get; set; }
+        public double DZD { get; set; }
+        public double EGP { get; set; }
+        public double ERN { get; set; }
+        public double ETB { get; set; }
+        public double EUR { get; set; }
+        public double FJD { get; set; }
+        public double FKP { get; set; }
+        public double GBP { get; set; }
+        public double GEL { get; set; }
+        public double GGP { get; set; }
+        public double GHS { get; set; }
+        public double GIP { get; set; }
+        public double GMD { get; set; }
+        public double GNF { get; set; }
+        public double GTQ { get; set; }
+        public double GYD { get; set; }
+        public double HKD { get; set; }
+        public double HNL { get; set; }
+        public double HRK { get; set; }
+        public double HTG { get; set; }
+        public double HUF { get; set; }
+        public double IDR { get; set; }
+        public double ILS { get; set; }
+        public double IMP { get; set; }
+        public double INR { get; set; }
+        public double IQD { get; set; }
+        public double IRR { get; set; }
+        public double ISK { get; set; }
+        public double JEP { get; set; }
+        public double JMD { get; set; }
+        public double JOD { get; set; }
+        public double JPY { get; set; }
+        public double KES { get; set; }
+        public double KGS { get; set; }
+        public double KHR { get; set; }
+        public double KMF { get; set; }
+        public double KPW { get; set; }
+        public double KRW { get; set; }
+        public double KWD { get; set; }
+        public double KYD { get; set; }
+        public double KZT { get; set; }
+        public double LAK { get; set; }
+        public double LBP { get; set; }
+        public double LKR { get; set; }
+        public double LRD { get; set; }
+        public double LSL { get; set; }
+        public double LTL { get; set; }
+        public double LVL { get; set; }
+        public double LYD { get; set; }
+        public double MAD { get; set; }
+        public double MDL { get; set; }
+        public double MGA { get; set; }
+        public double MKD { get; set; }
+        public double MMK { get; set; }
+        public double MNT { get; set; }
+        public double MOP { get; set; }
+        public double MRO { get; set; }
+        public double MUR { get; set; }
+        public double MVR { get; set; }
+        public double MWK { get; set; }
+        public double MXN { get; set; }
+        public double MYR { get; set; }
+        public double MZN { get; set; }
+        public double NAD { get; set; }
+        public double NGN { get; set; }
+        public double NIO { get; set; }
+        public double NOK { get; set; }
+        public double NPR { get; set; }
+        public double NZD { get; set; }
+        public double OMR { get; set; }
+        public double PAB { get; set; }
+        public double PEN { get; set; }
+        public double PGK { get; set; }
+        public double PHP { get; set; }
+        public double PKR { get; set; }
+        public double PLN { get; set; }
+        public double PYG { get; set; }
+        public double QAR { get; set; }
+        public double RON { get; set; }
+        public double RSD { get; set; }
+        public double RUB { get; set; }
+        public double RWF { get; set; }
+        public double SAR { get; set; }
+        public double SBD { get; set; }
+        public double SCR { get; set; }
+        public double SDG { get; set; }
+        public double SEK { get; set; }
+        public double SGD { get; set; }
+        public double SHP { get; set; }
+        public double SLL { get; set; }
+        public double SOS { get; set; }
+        public double SRD { get; set; }
+        public double STD { get; set; }
+        public double SVC { get; set; }
+        public double SYP { get; set; }
+        public double SZL { get; set; }
+        public double THB { get; set; }
+        public double TJS { get; set; }
+        public double TMT { get; set; }
+        public double TND { get; set; }
+        public double TOP { get; set; }
+        public double TRY { get; set; }
+        public double TTD { get; set; }
+        public double TWD { get; set; }
+        public double TZS { get; set; }
+        public double UAH { get; set; }
+        public double UGX { get; set; }
+        public double USD { get; set; }
+        public double UYU { get; set; }
+        public double UZS { get; set; }
+        public double VEF { get; set; }
+        public double VND { get; set; }
+        public double VUV { get; set; }
+        public double WST { get; set; }
+        public double XAF { get; set; }
+        public double XAG { get; set; }
+        public double XAU { get; set; }
+        public double XCD { get; set; }
+        public double XDR { get; set; }
+        public double XOF { get; set; }
+        public double XPF { get; set; }
+        public double YER { get; set; }
+        public double ZAR { get; set; }
+        public double ZMK { get; set; }
+        public double ZMW { get; set; }
+        public double ZWL { get; set; }
+    }
+
+    public class Valuta
+    {
+        public bool success { get; set; }
+        public int timestamp { get; set; }
+        public String @base { get; set; }
+        public String date { get; set; }
+        public Rates rates { get; set; }
+    }
+
+
+    public class Oppgave1
+    {
+        string valFra = "";
+        string valTil = "";
+        double belop = 0.0;
+        string[] valutaer = new string[] { "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN",
+            "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD", "CAD", "CDF", "CHF",
+            "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB",
+            "EUR", "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG",
+            "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF",
+            "KPW", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA",
+            "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR",
+            "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD",
+            "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND",
+            "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "VUV", "WST", "XAF", "XAG",
+            "XAU", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMK", "ZMW", "ZWL" };
+        string dato = "";
+        Valuta valuta = new Valuta();
+
+        Valuta getValutaer(string dato)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                //string url= "http://data.fixer.io/api/latest?access_key=6283daafaa3d1edc4ce4dab100ee8ee5";
+                string url = "http://data.fixer.io/api/" + dato + "?access_key=6283daafaa3d1edc4ce4dab100ee8ee5";
+                //url = "https://gnydn.com/latest.json";
+                var json = wc.DownloadString(url);
+                return JsonSerializer.Deserialize<Valuta>(json);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Oppgave1 p = new Oppgave1();
+
+            Console.WriteLine("Valutaomformer\r");
+            Console.WriteLine("------------------------\n");
+
+            bool fortsett = true;
+
+            while (fortsett)
+            {
+
+                while (p.valFra == "")
+                {
+                    // Be bruker skrive inn fra valuta.
+                    Console.WriteLine("Velg fra valuta:");
+                    p.valFra = Console.ReadLine().ToUpper();
+                    int pos = Array.IndexOf(p.valutaer, p.valFra);
+                    if (pos == -1)
+                    {
+                        Console.WriteLine("Følgende valuaer er tillatt: AED,AFN,ALL,AMD,ANG,AOA,ARS,AUD,AWG,AZN,BAM,BBD,BDT,BGN,BHD,BIF,BMD,BND,BOB,BRL,BSD,BTC,BTN,BWP,BYN,BYR,BZD,CAD,CDF,CHF,CLF,CLP,CNY,COP,CRC,CUC,CUP,CVE,CZK,DJF,DKK,DOP,DZD,EGP,ERN,ETB,EUR,FJD,FKP,GBP,GEL,GGP,GHS,GIP,GMD,GNF,GTQ,GYD,HKD,HNL,HRK,HTG,HUF,IDR,ILS,IMP,INR,IQD,IRR,ISK,JEP,JMD,JOD,JPY,KES,KGS,KHR,KMF,KPW,KRW,KWD,KYD,KZT,LAK,LBP,LKR,LRD,LSL,LTL,LVL,LYD,MAD,MDL,MGA,MKD,MMK,MNT,MOP,MRO,MUR,MVR,MWK,MXN,MYR,MZN,NAD,NGN,NIO,NOK,NPR,NZD,OMR,PAB,PEN,PGK,PHP,PKR,PLN,PYG,QAR,RON,RSD,RUB,RWF,SAR,SBD,SCR,SDG,SEK,SGD,SHP,SLL,SOS,SRD,STD,SVC,SYP,SZL,THB,TJS,TMT,TND,TOP,TRY,TTD,TWD,TZS,UAH,UGX,USD,UYU,UZS,VEF,VND,VUV,WST,XAF,XAG,XAU,XCD,XDR,XOF,XPF,YER,ZAR,ZMK,ZMW,ZWL");
+                        p.valFra = "";
+                    }
+
+                }
+
+
+                while (p.valTil == "")
+                {
+                    // Be bruker å velge til valuta.
+                    Console.WriteLine("Velg til valuta:");
+                    p.valTil = Console.ReadLine().ToUpper();
+                    int pos = Array.IndexOf(p.valutaer, p.valTil);
+                    if (pos == -1)
+                    {
+                        Console.WriteLine("Følgende valuaer er tillatt: AED,AFN,ALL,AMD,ANG,AOA,ARS,AUD,AWG,AZN,BAM,BBD,BDT,BGN,BHD,BIF,BMD,BND,BOB,BRL,BSD,BTC,BTN,BWP,BYN,BYR,BZD,CAD,CDF,CHF,CLF,CLP,CNY,COP,CRC,CUC,CUP,CVE,CZK,DJF,DKK,DOP,DZD,EGP,ERN,ETB,EUR,FJD,FKP,GBP,GEL,GGP,GHS,GIP,GMD,GNF,GTQ,GYD,HKD,HNL,HRK,HTG,HUF,IDR,ILS,IMP,INR,IQD,IRR,ISK,JEP,JMD,JOD,JPY,KES,KGS,KHR,KMF,KPW,KRW,KWD,KYD,KZT,LAK,LBP,LKR,LRD,LSL,LTL,LVL,LYD,MAD,MDL,MGA,MKD,MMK,MNT,MOP,MRO,MUR,MVR,MWK,MXN,MYR,MZN,NAD,NGN,NIO,NOK,NPR,NZD,OMR,PAB,PEN,PGK,PHP,PKR,PLN,PYG,QAR,RON,RSD,RUB,RWF,SAR,SBD,SCR,SDG,SEK,SGD,SHP,SLL,SOS,SRD,STD,SVC,SYP,SZL,THB,TJS,TMT,TND,TOP,TRY,TTD,TWD,TZS,UAH,UGX,USD,UYU,UZS,VEF,VND,VUV,WST,XAF,XAG,XAU,XCD,XDR,XOF,XPF,YER,ZAR,ZMK,ZMW,ZWL");
+                        p.valTil = "";
+                    }
+
+                }
+
+                while (p.belop == 0.0)
+                {
+                    Console.WriteLine("Velg belop:");
+                    string belopInn = Console.ReadLine();
+                    try
+                    {
+                        p.belop = Convert.ToDouble(belopInn);
+                    }
+                    catch (Exception)
+                    {
+                        p.belop = 0.0;
+                        Console.WriteLine("Skriv et gyldig beløp.");
+                    }
+                }
+
+
+
+                // Be bruke velge dato.
+                Console.WriteLine("Velg dato (ÅÅÅÅ-MM-DD). Trykk enter for å bruke dagens dato ");
+                p.dato = Console.ReadLine();
+                if (p.dato == "") p.dato = "latest";
+                p.valuta = p.getValutaer(p.dato);
+
+                // Regn ut valutakursen.
+                double val1 = Convert.ToDouble(p.valuta.rates.GetType().GetProperty(p.valTil).GetValue(p.valuta.rates, null));
+                double val2 = Convert.ToDouble(p.valuta.rates.GetType().GetProperty(p.valFra).GetValue(p.valuta.rates, null));
+                double res = Math.Round((p.belop * val1 / val2), 2);
+                Console.WriteLine($"{p.belop} {p.valFra} er {res} {p.valTil} den {p.valuta.date}.");
+                // Spør om å fortsette eller avslutte
+                Console.Write("Trykk X for å lukke eller noe annet for å starte på nytt");
+                ConsoleKeyInfo cki = Console.ReadKey();
+                if (cki.Key.ToString().ToUpper() == "X") fortsett = false;
+                p.valFra = ""; p.valTil = ""; p.belop = 0.0; p.dato = "";
+                Console.WriteLine("");
+
+            }
+        }
+    }
+}
